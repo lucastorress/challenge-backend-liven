299167231461ecaa5c0f32f15b92323b69f8ed10
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-06-23 20:04:33 -0300

    ✨ feat: add config typeorm

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import * as express from 'express';
 import router from './routes';
+import database from './database/PostgreSQLConnection';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+// establish database connection
+database
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
 
 const app = express();
 
