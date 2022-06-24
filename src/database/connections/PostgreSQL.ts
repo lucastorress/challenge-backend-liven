40707745dbf67b74490c88a79f79c24244b7ac93
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-06-24 03:30:06 -0300

    ⚡ chore: add singleton pattern to postgresql connector

--- src/database/connections/PostgreSQL.ts
+++ src/database/connections/PostgreSQL.ts
@@ -1,20 +1,31 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
-const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.TYPEORM_HOST,
-  port: +process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  synchronize: false,
-  logging: false,
-  entities: ['src/entity/*.ts'],
-  migrations: ['src/database/migrations/*.ts'],
-  subscribers: [],
-});
+class SingletonConnection {
+  private static _instance: DataSource;
 
-export default AppDataSource;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  public static getInstance(): DataSource {
+    if (!this._instance) {
+      this._instance = new DataSource({
+        type: 'postgres',
+        host: process.env.TYPEORM_HOST,
+        port: +process.env.TYPEORM_PORT,
+        username: process.env.TYPEORM_USERNAME,
+        password: process.env.TYPEORM_PASSWORD,
+        database: process.env.TYPEORM_DATABASE,
+        synchronize: false,
+        logging: false,
+        entities: ['src/entity/*.ts'],
+        migrations: ['src/database/migrations/*.ts'],
+        subscribers: [],
+      });
+    }
+    return this._instance;
+  }
+}
+
+export default SingletonConnection.getInstance();
