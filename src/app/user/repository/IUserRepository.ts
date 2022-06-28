fe690fb4808dce9642f8cbfa314cfe6730b9a18d
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-06-28 10:25:21 -0300

    ⚡ chore: update interface of user repository

--- src/app/user/repository/IUserRepository.ts
+++ src/app/user/repository/IUserRepository.ts
@@ -1,9 +1,9 @@
-import User from '../entity/props/User';
+import { User, UserProps } from '../entity/User';
 
 export default interface IUserRepository {
-  findUserById(id: string): Promise<User | undefined>;
-  findUserByEmail(email: string): Promise<User | undefined>;
-  saveUser(data: User): Promise<User>;
-  updateUser(id: string, data: User): Promise<User>;
+  findUserById(id: string): Promise<User | null>;
+  findUserByEmail(email: string): Promise<User | null>;
+  saveUser(user: UserProps): Promise<User>;
+  updateUser(id: string, user: UserProps): Promise<User>;
   removeUser(id: string): Promise<boolean>;
 }
